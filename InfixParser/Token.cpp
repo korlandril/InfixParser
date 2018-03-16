238c015fd53e794d7d01894aa819ef55f3859219
korlandril <taa522@mail.umkc.edu>
2018-03-15 20:24:27 -0500

    Add project files.

---
+++ InfixParser/Token.cpp
@@ -0,0 +1,76 @@
+#include "Token.h"
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+Token::Token(int inopd)
+{
+	opd = inopd;
+	opr = "";
+}
+
+Token::Token(string inopr)
+{
+	opr = inopr;
+	opd = NULL;
+}
+
+int Token::getopd()
+{
+	return opd;
+}
+
+string Token::getopr()
+{
+	return opr;
+}
+
+void Token::setopd(int inopd)
+{
+	opd = inopd;
+}
+
+void Token::setopr(string inopr)
+{
+	opr = inopr;
+}
+
+bool Token::isopd()
+{
+	if (opd != NULL)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool Token::isopr()
+{
+	if (opr.length() != 0) {
+		return true;
+	}
+	return false;
+}
+
+bool Token::is_binary()
+{
+	vector<string>::iterator it;
+	it = find(binary.begin(), binary.end(), opr);
+	if (it != binary.end())
+		return true;
+	else
+		return false;
+
+}
+
+bool Token::is_unary()
+{
+	vector<string>::iterator it;
+	it = find(unary.begin(), unary.end(), opr);
+	if (it != unary.end())
+		return true;
+	else
+		return false;
+}
---
+++ InfixParser/Token.h
@@ -0,0 +1,32 @@
+#ifndef TOKEN_H
+#include <string>
+#include <vector>
+using namespace std;
+class Token {
+	private:
+		int opd;
+		string opr; 
+
+		vector<string> unary = { "++", "--", "!"};
+		vector<string> binary = {"+", "-", "*", "/", "^", "%", ">", ">=", "<", "<=", "==", "!=", "&&", "||"};
+		char type;
+	public:
+		Token(int inopd);
+		Token(string inopr);
+
+		int getopd();
+		string getopr();
+
+		void setopd(int inopd);
+		void setopr(string inopr);
+
+		bool isopd();
+		bool isopr();
+		bool is_binary();
+		bool is_unary();
+		
+
+};
+
+#endif
+
